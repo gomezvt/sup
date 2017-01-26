21fa7d5b4f4929fd8a5b82225071def44d60766a
gomezvt <greg@gomez.ws>
2017-01-25 22:32:21 -0500

    Enable phone number dial

--- Classes/BVTYelpPhoneTableViewCell.m
+++ Classes/BVTYelpPhoneTableViewCell.m
@@ -10,7 +10,7 @@
 
 @interface BVTYelpPhoneTableViewCell ()
 
-@property (nonatomic, weak) IBOutlet UILabel *phoneNumberLabel;
+@property (nonatomic, weak) IBOutlet UIButton *phoneNumberButton;
 @property (nonatomic, weak) IBOutlet UIImageView *PhoneImageView;
 
 @end
@@ -25,7 +25,28 @@
 - (void)setSelectedBusiness:(YLPBusiness *)selectedBusiness
 {
     _selectedBusiness = selectedBusiness;
-    self.phoneNumberLabel.text = self.selectedBusiness.phone;
+    
+    [self.phoneNumberButton setTitle:selectedBusiness.phone forState:UIControlStateNormal];
+}
+
+- (IBAction)didTapPhoneNumberButton:(id)sender
+{
+    NSString *phoneNumber;
+    if ([self.selectedBusiness.phone hasPrefix:@"+"])
+    {
+        phoneNumber = [self.selectedBusiness.phone substringFromIndex:1];
+    }
+    else
+    {
+        phoneNumber = self.selectedBusiness.phone;
+    }
+    
+    NSString *phoneString = [NSString stringWithFormat:@"telprompt:%@", phoneNumber];
+    NSURL *phoneURL = [NSURL URLWithString:phoneString];
+    
+    [[UIApplication sharedApplication] openURL:phoneURL options:@{} completionHandler:^(BOOL success) {
+        NSLog(@"");
+    }];
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
