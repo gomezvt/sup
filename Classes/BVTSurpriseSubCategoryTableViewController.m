f9f239d39bb8c22e4f2218a5ed70b93586b67ef9
gomezvt <greg@gomez.ws>
2017-05-29 23:24:25 -0400

    fix predicate

--- Classes/BVTSurpriseSubCategoryTableViewController.m
+++ Classes/BVTSurpriseSubCategoryTableViewController.m
@@ -269,7 +269,7 @@ static NSString *const kCheckMarkGraphic = @"green_check";
     cell.textLabel.numberOfLines = 0;
     
     NSArray *array = [self.catDict valueForKey:self.categoryTitle];
-    NSString *btitle = [[array filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"self MATCHES[c] %@", title]] lastObject];
+    NSString *btitle = [[array filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"self == %@", title]] lastObject];
     if (!btitle)
     {
         cell.accessoryView = nil;
