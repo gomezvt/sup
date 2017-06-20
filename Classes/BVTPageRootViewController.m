5448fe1a070ff7e1c8d124e30899471e03a54fc2
gomezvt <greg@gomez.ws>
2017-06-20 01:35:21 -0400

    tweaks

--- Classes/BVTPageRootViewController.m
+++ Classes/BVTPageRootViewController.m
@@ -68,7 +68,7 @@
         [self.pageViewController setViewControllers:viewControllers direction:UIPageViewControllerNavigationDirectionForward animated:NO completion:nil];
         
         // Change the size of page view controller
-        self.pageViewController.view.frame = CGRectMake(0, -50.f, self.view.frame.size.width, self.view.frame.size.height + 30.f);
+        self.pageViewController.view.frame = CGRectMake(0, -50.f, self.view.frame.size.width, self.view.frame.size.height + 25.f);
         
         [self addChildViewController:_pageViewController];
         [self.view addSubview:_pageViewController.view];
