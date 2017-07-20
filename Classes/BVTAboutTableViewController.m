7c0ff6d90f35e37497c32244d47a40ab955c4b28
gomezvt <greg@gomez.ws>
2017-07-19 22:24:44 -0400

    Update version number, add small tweaks

--- Classes/BVTAboutTableViewController.m
+++ Classes/BVTAboutTableViewController.m
@@ -170,7 +170,7 @@ static NSString *const kAboutTableViewNib = @"BVTAboutTableViewCell";
         else if (indexPath.row == 3)
         {
             cell.selectionStyle = UITableViewCellSelectionStyleNone;
-            cell.textLabel.text = @"Version 2.0.2";
+            cell.textLabel.text = @"Version 2.0.3";
         }
         else
         {
@@ -187,12 +187,12 @@ static NSString *const kAboutTableViewNib = @"BVTAboutTableViewCell";
         else if (indexPath.row == 1)
         {
             cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-            cell.textLabel.text = @"Write a review or rate us on the App Store";
+            cell.textLabel.text = @"Write a review or rate us on the App Store!";
         }
         else
         {
             cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-            cell.textLabel.text = @"Send an email";
+            cell.textLabel.text = @"Send Greg an email";
         }
     }
     
