fb812c71d8fc139ca63ed9b7987a3e4b7f3ef453
gomezvt <greg@gomez.ws>
2018-02-28 19:57:53 -0500

    Create Favorites cell for inclusion in DetailTableViewController

---
+++ SUPFavoritesCell.h
@@ -0,0 +1,13 @@
+//
+//  SUPFavoritesCell.h
+//  sup
+//
+//  Created by Greg on 2/28/18.
+//  Copyright © 2018 gms. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SUPFavoritesCell : UITableViewCell
+
+@end
---
+++ SUPFavoritesCell.m
@@ -0,0 +1,24 @@
+//
+//  SUPFavoritesCell.m
+//  sup
+//
+//  Created by Greg on 2/28/18.
+//  Copyright © 2018 gms. All rights reserved.
+//
+
+#import "SUPFavoritesCell.h"
+
+@implementation SUPFavoritesCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
