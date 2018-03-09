bf13a9333aff1ddc91a099bedcae0445f59e8cc2
gomezvt <greg@gomez.ws>
2018-03-08 22:49:48 -0500

    Add logic back in to store biz photos, open/close, prices etc. lost by updating pods.

--- Pods/YelpAPI/Classes/Response/YLPBusiness.m
+++ Pods/YelpAPI/Classes/Response/YLPBusiness.m
@@ -7,10 +7,12 @@
 //
 
 #import "YLPBusiness.h"
+
 #import "YLPCategory.h"
 #import "YLPCoordinate.h"
 #import "YLPLocation.h"
 #import "YLPResponsePrivate.h"
+#import <CoreLocation/CoreLocation.h>
 
 @implementation YLPBusiness
 
@@ -63,24 +65,64 @@
 
 - (instancetype)initWithDictionary:(NSDictionary *)businessDict {
     if (self = [super init]) {
-        _closed = [businessDict[@"is_closed"] boolValue];
-
-        _URL = [[NSURL alloc] initWithString:businessDict[@"url"]];
-        NSString *imageURLString = [businessDict ylp_objectMaybeNullForKey:@"image_url"];
-        _imageURL = imageURLString.length > 0 ? [[NSURL alloc] initWithString:imageURLString] : nil;
         
+        NSString *phone = [businessDict ylp_objectMaybeNullForKey:@"phone"];
+        NSString *imageURLString = [businessDict ylp_objectMaybeNullForKey:@"image_url"];
+        
+        _closed = [businessDict[@"is_closed"] boolValue];
+        if (businessDict[@"url"])
+        {
+            _URL = [[NSURL alloc] initWithString:businessDict[@"url"]];
+        }
+        
+        id reviews = businessDict[@"reviews"];
+        if (reviews)
+        {
+            if ([reviews isKindOfClass:[NSArray class]])
+            {
+                _reviews = reviews;
+            }
+        }
+        
+        id photos = businessDict[@"photos"];
+        if (photos)
+        {
+            if ([photos isKindOfClass:[NSArray class]])
+            {
+                _photos = photos;
+            }
+        }
+        _imageURL = imageURLString.length > 0 ? [[NSURL alloc] initWithString:imageURLString] : nil;
         _rating = [businessDict[@"rating"] doubleValue];
         _reviewCount = [businessDict[@"review_count"] integerValue];
-        
         _name = businessDict[@"name"];
         _identifier = businessDict[@"id"];
-        NSString *phone = [businessDict ylp_objectMaybeNullForKey:@"phone"];
         _phone = phone.length > 0 ? phone : nil;
+        _price = businessDict[@"price"];
+        _open_now = businessDict[@"is_open_now"];
+        // BusinessWithID returned values
+        self.hoursItem = businessDict[@"hours"];
+        if ([self.hoursItem isKindOfClass:[NSArray class]])
+        {
+            NSArray *hoursArray = (NSArray *)self.hoursItem;
+            if ([[hoursArray lastObject] isKindOfClass:[NSDictionary class]])
+            {
+                NSDictionary *hoursDict = [hoursArray lastObject];
+                id value = hoursDict[@"is_open_now"];
+                if ([value isKindOfClass:[NSNumber class]])
+                {
+                    _isOpenNow = [value boolValue];
+                }
+                
+                _businessHours = hoursDict[@"open"];
+            }
+        }
         
         _categories = [self.class categoriesFromJSONArray:businessDict[@"categories"]];
         YLPCoordinate *coordinate = [self.class coordinateFromJSONDictionary:businessDict[@"coordinates"]];
         _location = [[YLPLocation alloc] initWithDictionary:businessDict[@"location"] coordinate:coordinate];
     }
+    
     return self;
 }
 
@@ -104,3 +146,4 @@
 }
 
 @end
+
